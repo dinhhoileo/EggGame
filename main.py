7a5122c6811455f40802cc3f039c870ab9334a74
dinhhoileo <huynhhoiop@gmail.com>
2023-09-08 16:15:48 +0700

    update

--- main.py
+++ main.py
@@ -17,25 +17,41 @@ class Event:
         self.limit_right=650
         
     def coin_fall(self):
-        # base case
-        self.canvas.move(self.coin,0,10) # tọa độ rơi(x;y)
-        self.y+=10
-        if self.y >=550: # giới hạn rơi xuống cỡ 340
-            self.y= 5 # vị trí cách trục y =5
-            self.x=randint(2,750) #ramdom vị trí rơi
-            self.canvas.coords(self.coin,self.x,self.y) # cập nhật vị trí đồng xu
-        self.canvas.after(50,self.coin_fall) # gọi đệ quy tiếp tục rơi
+        self.canvas.move(self.coin, 0, 10)
+        self.y += 10
+
+        if self.y >= 550:
+            self.y = 5
+            self.x = randint(2, 750)
+            self.canvas.coords(self.coin, self.x, self.y)
+
+        self.coin_coords_x = self.canvas.coords(self.coin)[0]
+        self.hand_coords_x = self.canvas.coords(self.hand)[0]
+        self.coin_coords_y = self.canvas.coords(self.coin)[1]
+        self.hand_coords_y = self.canvas.coords(self.hand)[1]
+
+        self.score = 0 
+        self.score_entry= Entry(self.game,width=5,textvariable=str(self.score),bg="blue")
+        self.score_entry.place(x=750,y=5)
+        if  self.coin_coords_x+50 >= self.hand_coords_x and self.coin_coords_x+50 <=self.hand_coords_x +178 and self.coin_coords_y+50 >=self.hand_coords_y and self.coin_coords_y <=self.hand_coords_y+100:
+
+            self.canvas.delete("coin")
+            self.y = 5
+            self.x = randint(2, 750)
+            self.canvas.coords(self.coin, self.x, self.y)
+
+        self.canvas.after(100, self.coin_fall)
 
     def right(self):
         self.current_x=self.canvas.coords(self.hand)[0] # lấy vị trí của bàn tay hiện tại và thay đổi trục x
         if self.current_x <= self.limit_right:
-            self.canvas.move(self.hand,20,0)
+            self.canvas.move(self.hand,30,0)
             self.canvas.update()
 
     def left(self):
         self.current_y=self.canvas.coords(self.hand)[0] # lấy vị trí của bàn tay hiện tại và thay đổi trục current_y
         if self.current_y > self.limit_left:
-            self.canvas.move(self.hand,-20,0)
+            self.canvas.move(self.hand,-30.56412,0)
             self.canvas.update()
             
     def move_hand(self,event):
@@ -61,7 +77,7 @@ class Game:
         self.img[1]=ImageTk.PhotoImage(file="coin.png")
         self.img[2]=ImageTk.PhotoImage(file="hand.png")
         
-        self.background = self.canvas.create_image(0,0,anchor=NW,image=self.img[0])
+        # self.background = self.canvas.create_image(0,0,anchor=NW,image=self.img[0])
         self.coin = self.canvas.create_image(self.x,self.y,anchor=NW,image=self.img[1])
         self.hand =self.canvas.create_image(0,340,anchor=NW,image=self.img[2])
         self.canvas.update()
